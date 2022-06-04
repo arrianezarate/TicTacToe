0db5728b769712e3a7dade2f1b020c029a512f59
Arriane Nicole Zarate <arrianenic.z@gmail.com>
2022-06-04 16:30:07 +0800

    button 1 is working

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tictac1 = new System.Windows.Forms.Button();
+            this.tictac3 = new System.Windows.Forms.Button();
+            this.tictac4 = new System.Windows.Forms.Button();
+            this.tictac2 = new System.Windows.Forms.Button();
+            this.tictac5 = new System.Windows.Forms.Button();
+            this.tictac6 = new System.Windows.Forms.Button();
+            this.tictac7 = new System.Windows.Forms.Button();
+            this.tictac8 = new System.Windows.Forms.Button();
+            this.tictac9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tictac1
+            // 
+            this.tictac1.Location = new System.Drawing.Point(145, 82);
+            this.tictac1.Name = "tictac1";
+            this.tictac1.Size = new System.Drawing.Size(97, 72);
+            this.tictac1.TabIndex = 0;
+            this.tictac1.UseVisualStyleBackColor = true;
+            this.tictac1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tictac3
+            // 
+            this.tictac3.Location = new System.Drawing.Point(558, 82);
+            this.tictac3.Name = "tictac3";
+            this.tictac3.Size = new System.Drawing.Size(97, 72);
+            this.tictac3.TabIndex = 2;
+            this.tictac3.UseVisualStyleBackColor = true;
+            // 
+            // tictac4
+            // 
+            this.tictac4.Location = new System.Drawing.Point(145, 201);
+            this.tictac4.Name = "tictac4";
+            this.tictac4.Size = new System.Drawing.Size(97, 72);
+            this.tictac4.TabIndex = 3;
+            this.tictac4.UseVisualStyleBackColor = true;
+            // 
+            // tictac2
+            // 
+            this.tictac2.Location = new System.Drawing.Point(359, 82);
+            this.tictac2.Name = "tictac2";
+            this.tictac2.Size = new System.Drawing.Size(97, 72);
+            this.tictac2.TabIndex = 4;
+            this.tictac2.UseVisualStyleBackColor = true;
+            this.tictac2.Click += new System.EventHandler(this.tictac2_Click);
+            // 
+            // tictac5
+            // 
+            this.tictac5.Location = new System.Drawing.Point(357, 201);
+            this.tictac5.Name = "tictac5";
+            this.tictac5.Size = new System.Drawing.Size(97, 72);
+            this.tictac5.TabIndex = 5;
+            this.tictac5.UseVisualStyleBackColor = true;
+            // 
+            // tictac6
+            // 
+            this.tictac6.Location = new System.Drawing.Point(551, 201);
+            this.tictac6.Name = "tictac6";
+            this.tictac6.Size = new System.Drawing.Size(97, 72);
+            this.tictac6.TabIndex = 6;
+            this.tictac6.UseVisualStyleBackColor = true;
+            // 
+            // tictac7
+            // 
+            this.tictac7.Location = new System.Drawing.Point(145, 322);
+            this.tictac7.Name = "tictac7";
+            this.tictac7.Size = new System.Drawing.Size(97, 72);
+            this.tictac7.TabIndex = 7;
+            this.tictac7.UseVisualStyleBackColor = true;
+            // 
+            // tictac8
+            // 
+            this.tictac8.Location = new System.Drawing.Point(358, 322);
+            this.tictac8.Name = "tictac8";
+            this.tictac8.Size = new System.Drawing.Size(97, 72);
+            this.tictac8.TabIndex = 8;
+            this.tictac8.UseVisualStyleBackColor = true;
+            // 
+            // tictac9
+            // 
+            this.tictac9.Location = new System.Drawing.Point(551, 322);
+            this.tictac9.Name = "tictac9";
+            this.tictac9.Size = new System.Drawing.Size(97, 72);
+            this.tictac9.TabIndex = 9;
+            this.tictac9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tictac9);
+            this.Controls.Add(this.tictac8);
+            this.Controls.Add(this.tictac7);
+            this.Controls.Add(this.tictac6);
+            this.Controls.Add(this.tictac5);
+            this.Controls.Add(this.tictac2);
+            this.Controls.Add(this.tictac4);
+            this.Controls.Add(this.tictac3);
+            this.Controls.Add(this.tictac1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button tictac1;
+        private System.Windows.Forms.Button tictac3;
+        private System.Windows.Forms.Button tictac4;
+        private System.Windows.Forms.Button tictac2;
+        private System.Windows.Forms.Button tictac5;
+        private System.Windows.Forms.Button tictac6;
+        private System.Windows.Forms.Button tictac7;
+        private System.Windows.Forms.Button tictac8;
+        private System.Windows.Forms.Button tictac9;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,9 +12,32 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        String Btntictac = "X";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tictac1.Text == "")
+            {
+                tictac1.Text = Btntictac;
+                if (Btntictac == "X")
+                {
+                    Btntictac = "O";
+                }
+                else
+                {
+                    Btntictac = "X";
+                }
+            }
+        }
+
+        private void tictac2_Click(object sender, EventArgs e)
+        {
+            
+            
         }
     }
 }
